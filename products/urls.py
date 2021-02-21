5e8b813b5409fdb82c7c77d2439b1b1a25902963
jet-c-21 <67193495+jet-c-21@users.noreply.github.com>
2021-02-21 17:00:16 +0800

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -28,7 +28,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('', product_list_view, name='product-list'),
-    path('create/', product_create_view, name='product-list'),
+    path('create/', product_create_view, name='product-create'),
     path('<int:product_id>/', product_detail_view, name='product-detail'),
     path('<int:product_id>/update/', product_update_view, name='product-update'),
     path('<int:product_id>/delete/', product_delete_view, name='product-delete'),
